2511516149b7848127515b466477484fa70f2807
FunbiOyede <oyedetobi49@gmail.com>
2020-01-09 18:00:41 +0100

    routes for backend

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const bodyParser = require("body-parser");
+const { MongoClient, ObjectId } = require("mongodb");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT = 5000;
+const server = http.createServer(app);
+MongoClient.connect(
+  "mongodb://localhost:27017",
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      return err;
+    }
+    const db = client.db("Todo");
+    // add a new todo
+    app.post("/todos", (req, res) => {
+      const { task } = req.body;
+      const { status } = req.body;
+      db.collection("Todos")
+        .insertOne({
+          todo: task,
+          status: status
+        })
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(400).json(err));
+    });
+
+    // get all todos
+    app.get("/todos", (req, res) => {
+      db.collection("Todos")
+        .find()
+        .toArray()
+        .then(doc => res.status(200).json(doc))
+        .catch(err => res.status(400).json(err));
+    });
+
+    // update todo
+    app.put("/todos/:id", (req, res) => {
+      const { id } = req.params;
+      const { status } = req.body;
+      db.collection("Todos")
+        .findOneAndUpdate({ _id: ObjectId(id) }, { $set: { status: status } })
+        .then(() => res.status(200).json("Todo Updated"))
+        .catch(err => res.status(400).json(err));
+    });
+
+    // delete todo
+    app.delete("/todos/:id", (req, res) => {
+      const { id } = req.params;
+      db.collection("Todos")
+        .findOneAndDelete({ _id: ObjectId(id) })
+        .then(() => res.status(200).json("todo deleted"))
+        .catch(err => res.status(200).json(err));
+    });
+  }
+);
+server.listen(PORT, () => {
+  console.log(`sever ready on ${PORT}`);
+});
